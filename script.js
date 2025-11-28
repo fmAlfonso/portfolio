b26becdb5c0c676256641d04edf167a5365e0920
FMalfonso <163765926+fmAlfonso@users.noreply.github.com>
2025-11-28 13:16:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ const body = document.body;
 
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark');
+
+    toggleButton.textContent = body.classList.contains('dark') ? 'Switch to Light Mode' : 'Switch to Dark Mode';
 });
