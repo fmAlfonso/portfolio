ac3ee5346792031b0e524993e92c03b5604708f3
FMalfonso <163765926+fmAlfonso@users.noreply.github.com>
2025-11-28 13:24:23 +0800

    Update toggle button text based on theme

--- script.js
+++ script.js
@@ -4,5 +4,4 @@ const body = document.body;
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark');
 
-    toggleButton.textContent = body.classList.contains('dark') ? 'Switch to Light Mode' : 'Switch to Dark Mode';
 });
