2740cbae311df2f309ec16d9adc74798bfb5184f
FMalfonso <163765926+fmAlfonso@users.noreply.github.com>
2025-12-12 14:19:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
 
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark');
+}
+
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark');
 
+    if (body.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
 });
